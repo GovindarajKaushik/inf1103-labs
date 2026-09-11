1f48b01eb555cff62eada50f9e5638c0765b67b2
Kaushik <kaushik20004@gmail.com>
2026-09-11 11:34:19 +0800

    Handle invalid input

--- auditor.py
+++ auditor.py
@@ -5,6 +5,12 @@ total_inventory = 0
 while True:
     inventory = input("Please Enter Stock quantity: ")
 
-
-    if str(inventory) == "quit":
+    if inventory.lower() == "quit":
+        print(f"Total inventory: {total_inventory}, rejected entries: {rejected_inventory}")
         break
+
+    elif not inventory.isdigit():
+        rejected_inventory += 1
+        print("Please enter a valid number.")
+        continue
+
