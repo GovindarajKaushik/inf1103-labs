6346c6620d86adea1bfddd5f59f701fd11505056
Kaushik <govindarajkaushik24@gmail.com>
2026-09-19 15:46:39 +0800

    Created a main() function to remove global variables

--- auditor.py
+++ auditor.py
@@ -1,6 +1,5 @@
 # Lab 03 
 
-total_inventory = 0
 
 # Handles the prompt, handles input validation, and returns a valid integer or a "quit" signal
 def get_valid_input():
@@ -31,21 +30,31 @@ def calculate_tax(amount):
 def generate_report(total_inventory, rejected_inventory):
     print(f"Total inventory: {total_inventory}, rejected entries: {rejected_inventory}")
 
-while True:
-    inventory = get_valid_input()
-    # quit the loop and prints summary
-    if inventory == "quit":
-        generate_report(total_inventory, rejected_inventory)
-        break
-    # count rejected attempts
-    if inventory == "error":
-        rejected_inventory + 1
-        continue
-    # count total_inventory
-    total_inventory = process_delivery(total_inventory, inventory)
-    # Calculates tax
-    tax = calculate_tax(inventory)
-    # print warning and break loop when total inventory is more than 500
-    if total_inventory > 500:
-        print(f"ALERT! Total inventory exceeds 500 units. Currently at: {total_inventory}")
-        break
+# main function to remove global variables
+def main():
+    total_inventory = 0
+    rejected_inventory = 0
+
+    while True:
+        inventory = get_valid_input()
+        #quit the loop and prints summary
+        if inventory == "quit":
+            generate_report(total_inventory, rejected_inventory)
+            break
+        #count rejected attempts
+        if inventory == "error":
+            rejected_inventory += 1
+            continue
+        #calculate tax 10% for each delivery
+        tax = calculate_tax(inventory)
+        #count total_inventory
+        total_inventory = process_delivery(total_inventory, inventory)
+        # print warning and break loop when total inventory is more than 500
+        if total_inventory > 500:
+            print(f"ALERT! Total inventory exceeds 500 units. Currently at: {total_inventory}")
+            break
+
+
+main()
+
+
